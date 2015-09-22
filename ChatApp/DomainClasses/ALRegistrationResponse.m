0572d5e82f15e7670402c6f84b8cf4f67211c759
Adarsh <adarsh@applozic.com>
2015-09-22 21:08:33 +0530

    #4 Login

--- ChatApp/DomainClasses/ALRegistrationResponse.m
+++ ChatApp/DomainClasses/ALRegistrationResponse.m
@@ -10,4 +10,15 @@
 
 @implementation ALRegistrationResponse
 
+
+- (id)initWithJSONString:(NSString *)registrationResponse {
+
+    //TODO: Right now error is coming super initWithJSONString, so overriding it...once fixed remove this
+    self.message = [registrationResponse valueForKey:@"message"];
+    self.deviceKeyString = [registrationResponse valueForKey:@"deviceKeyString"];
+    self.suUserKeyString = [registrationResponse valueForKey:@"suUserKeyString"];
+    self.contactNumber = [registrationResponse valueForKey:@"contactNumber"];
+    self.lastSyncTime = [registrationResponse valueForKey:@"lastSyncTime"];
+    return self;
+}
 @end
--- ChatApp/DomainClasses/ALUser.m
+++ ChatApp/DomainClasses/ALUser.m
@@ -10,5 +10,6 @@
 
 @implementation ALUser
 
+
 @end
 
