547e06aa25623abd323716fedfcf59fe8452830e
Devashish Datt Mamgain <devashish.mamgain@mobitexter.net>
2015-11-28 15:05:56 +0530

    #84 checking for message type

--- sample-with-framework/Applozic/ALMQTTConversationService.m
+++ sample-with-framework/Applozic/ALMQTTConversationService.m
@@ -64,22 +64,28 @@ static MQTTSession *session;
 
 - (void)newMessage:(MQTTSession *)session data:(NSData *)data onTopic:(NSString *)topic qos:(MQTTQosLevel)qos retained:(BOOL)retained mid:(unsigned int)mid
 {
-    /*NSString *fullMessage = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"MQTT got new message: %@", fullMessage);*/
+    NSString *fullMessage = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"MQTT got new message: %@", fullMessage);
     
     NSError *error = nil;
     NSDictionary *theMessageDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-    NSString *messageJson = [theMessageDict objectForKey:@"message"];
+    NSString *type = [theMessageDict objectForKey:@"type"];
     
-    NSData *messageData = [messageJson
-                    dataUsingEncoding:NSUTF8StringEncoding];
-    
-    id result = [NSJSONSerialization JSONObjectWithData:messageData
-                                                options:NSJSONReadingAllowFragments
-                                                  error:NULL];
-    ALMessage *alMessage = [[ALMessage alloc] initWithDictonary:result];
-    [self.mqttConversationDelegate syncCall: alMessage];
-}
+    if ([type isEqualToString:@"MESSAGE_DELIVERED_READ"]) {
+        NSLog(@"mark as read and delivered");
+    } else if ([type isEqualToString: @"MESSAGE_RECEIVED"]) {
+        NSString *messageJson = [theMessageDict objectForKey:@"message"];
+        
+        NSData *messageData = [messageJson
+                               dataUsingEncoding:NSUTF8StringEncoding];
+        
+        id result = [NSJSONSerialization JSONObjectWithData:messageData
+                                                    options:NSJSONReadingAllowFragments
+                                                      error:NULL];
+        ALMessage *alMessage = [[ALMessage alloc] initWithDictonary:result];
+        [self.mqttConversationDelegate syncCall: alMessage];
+    }
+   }
 
 - (void)subAckReceived:(MQTTSession *)session msgID:(UInt16)msgID grantedQoss:(NSArray *)qoss
 {
