85dae8ce3b95436964a26d9cb124f3d35b55c79b
abhishek <abhishekthapliyal7579@gmail.com>
2015-12-28 19:12:05 +0530

    #172 Group Messaging Files Added

--- sample-with-framework/Applozic/ALGroupDBService.h
+++ sample-with-framework/Applozic/ALGroupDBService.h
@@ -10,4 +10,19 @@
 
 @interface ALGroupDBService : NSObject
 
+@property (nonatomic, strong) NSString *id;
+@property (nonatomic, strong) NSString *createdAt;
+@property (nonatomic, strong) NSString *displayName;
+@property (nonatomic, strong) NSString *channelKey;
+@property (nonatomic, strong) NSString *applicationId;
+@property (nonatomic, strong) NSString *type;
+@property (nonatomic, strong) NSString *updatedAt;
+@property (nonatomic, strong) NSString *userCount;
+@property (nonatomic, strong) NSString *messageSize;
+@property (nonatomic, strong) NSString *messageCount;
+@property (nonatomic, strong) NSString *userId;
+@property (nonatomic, strong) NSString *status;
+@property (nonatomic, strong) NSString *unreadCount;
+@property (nonatomic, strong) NSString *key;
+
 @end
