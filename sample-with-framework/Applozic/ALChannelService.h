089e358b3332e0ef414ff4159120cff63949caed
abhishek <abhishekthapliyal7579@gmail.com>
2016-01-04 15:30:08 +0530

    #179 Channel Updates : Database and Server Code Updates PART 2
    ALChannelService New File Added in ALChannel

---
+++ sample-with-framework/Applozic/ALChannelService.h
@@ -0,0 +1,13 @@
+//
+//  ALChannelService.h
+//  Applozic
+//
+//  Created by devashish on 04/01/2016.
+//  Copyright © 2016 applozic Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALChannelService : NSObject
+
+@end
---
+++ sample-with-framework/Applozic/ALChannelService.m
@@ -0,0 +1,13 @@
+//
+//  ALChannelService.m
+//  Applozic
+//
+//  Created by devashish on 04/01/2016.
+//  Copyright © 2016 applozic Inc. All rights reserved.
+//
+
+#import "ALChannelService.h"
+
+@implementation ALChannelService
+
+@end
