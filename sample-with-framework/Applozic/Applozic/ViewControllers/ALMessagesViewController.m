5476f267095e0b37121d058eadb4ca7b7297147c
abhishek <abhishekthapliyal7579@gmail.com>
2016-01-07 16:33:21 +0530

    #191 App local Notification : ALAppLocalNotification Class

--- sample-with-framework/Applozic/Applozic/ViewControllers/ALMessagesViewController.m
+++ sample-with-framework/Applozic/Applozic/ViewControllers/ALMessagesViewController.m
@@ -30,7 +30,6 @@
 #import "ALMQTTConversationService.h"
 #import "ALApplozicSettings.h"
 #import "ALDataNetworkConnection.h"
-#import "Reachability.h"
 #import "ALUserService.h"
 #import "ALChannelDBService.h"
 #import "ALChannel.h"
@@ -62,14 +61,6 @@
 @property (weak, nonatomic) IBOutlet UITableView *mTableView;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *mTableViewTopConstraint;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *mActivityIndicator;
-
-
--(void)reachabilityChanged:(NSNotification*)note;
-
-@property(strong) Reachability * googleReach;
-@property(strong) Reachability * localWiFiReach;
-@property(strong) Reachability * internetConnectionReach;
-
 
 // Private Variables
 @property (nonatomic) NSInteger mqttRetryCount;
@@ -138,7 +129,6 @@ ALMQTTConversationService *alMqttConversationService;
     [self.dataAvailablityLabel setTextColor:[UIColor whiteColor]];
     [self.view addSubview:self.dataAvailablityLabel];
     
-    [self viewDidLoadPart];
 }
 
 -(void) viewDidDisappear:(BOOL)animated
@@ -146,126 +136,6 @@ ALMQTTConversationService *alMqttConversationService;
     //    dispatch_async(dispatch_get_main_queue(), ^{
     //        [alMqttConversationService unsubscribeToConversation];
     //    });
-    
-}
-
--(void)viewDidLoadPart
-{
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
-    
-    // create a Reachability object for www.google.com
-    
-    self.googleReach = [Reachability reachabilityWithHostname:@"www.google.com"];
-    
-    self.googleReach.reachableBlock = ^(Reachability * reachability)
-    {
-        NSString * temp = [NSString stringWithFormat:@"GOOGLE Block Says Reachable(%@)", reachability.currentReachabilityString];
-        // NSLog(@"%@", temp);
-        
-        // to update UI components from a block callback
-        // you need to dipatch this to the main thread
-        // this uses NSOperationQueue mainQueue
-        
-    };
-    
-    self.googleReach.unreachableBlock = ^(Reachability * reachability)
-    {
-        NSString * temp = [NSString stringWithFormat:@"GOOGLE Block Says Unreachable(%@)", reachability.currentReachabilityString];
-        //  NSLog(@"%@", temp);
-        
-        // to update UI components from a block callback
-        // you need to dipatch this to the main thread
-        // this one uses dispatch_async they do the same thing (as above)
-        
-    };
-    
-    [self.googleReach startNotifier];
-    
-    // create a reachability for the local WiFi
-    
-    self.localWiFiReach = [Reachability reachabilityForLocalWiFi];
-    
-    // we ONLY want to be reachable on WIFI - cellular is NOT an acceptable connectivity
-    self.localWiFiReach.reachableOnWWAN = NO;
-    
-    self.localWiFiReach.reachableBlock = ^(Reachability * reachability)
-    {
-        NSString * temp = [NSString stringWithFormat:@"LocalWIFI Block Says Reachable(%@)", reachability.currentReachabilityString];
-        // NSLog(@"%@", temp);
-        
-        
-    };
-    
-    self.localWiFiReach.unreachableBlock = ^(Reachability * reachability)
-    {
-        NSString * temp = [NSString stringWithFormat:@"LocalWIFI Block Says Unreachable(%@)", reachability.currentReachabilityString];
-        
-        // NSLog(@"%@", temp);
-        
-    };
-    
-    [self.localWiFiReach startNotifier];
-    
-    // create a Reachability object for the internet
-    
-    self.internetConnectionReach = [Reachability reachabilityForInternetConnection];
-    
-    self.internetConnectionReach.reachableBlock = ^(Reachability * reachability)
-    {
-        NSString * temp = [NSString stringWithFormat:@" InternetConnection Says Reachable(%@)", reachability.currentReachabilityString];
-        // NSLog(@"%@", temp);
-    };
-    
-    self.internetConnectionReach.unreachableBlock = ^(Reachability * reachability)
-    {
-        NSString * temp = [NSString stringWithFormat:@"InternetConnection Block Says Unreachable(%@)", reachability.currentReachabilityString];
-        //  NSLog(@"%@", temp);
-    };
-    
-    [self.internetConnectionReach startNotifier];
-    
-}
-
--(void)reachabilityChanged:(NSNotification*)note
-{
-    Reachability * reach = [note object];
-    
-    if(reach == self.googleReach)
-    {
-        if([reach isReachable])
-        {
-            //NSLog(@"========== IF googleReach ============");
-        }
-        else
-        {
-            // NSLog(@"========== ELSE googleReach ============");
-        }
-    }
-    else if (reach == self.localWiFiReach)
-    {
-        if([reach isReachable])
-        {
-            // NSLog(@"========== IF localWiFiReach ============");
-        }
-        else
-        {
-            // NSLog(@"========== ELSE localWiFiReach ============");
-        }
-    }
-    else if (reach == self.internetConnectionReach)
-    {
-        if([reach isReachable])
-        {
-            // NSLog(@"========== IF internetConnectionReach ============");
-            [ALMessageService processLatestMessagesGroupByContact];
-            //changes required
-        }
-        else
-        {
-            //NSLog(@"========== ELSE internetConnectionReach ============");
-        }
-    }
     
 }
 
