46bc9a9679428c2804918077250804ef665b439b
Adarsh <adarsh@applozic.com>
2016-01-15 19:27:35 +0530

    #199  Demo UI changes

--- sample-with-framework/applozicdemo/ApplozicLoginViewController.m
+++ sample-with-framework/applozicdemo/ApplozicLoginViewController.m
@@ -165,18 +165,24 @@
     [user setUserId:[self.userIdField text]];
     [user setEmailId:[self.emailField text]];
     [user setPassword:[self.passwordField text]];
+    
+    [ALUserDefaultsHandler setApplicationKey:user.applicationId];
+    [ALUserDefaultsHandler setUserId:user.userId];
+    [ALUserDefaultsHandler setEmailId:user.emailId];
+
+
     [self.mActivityIndicator startAnimating];
     //mbChatManger...
-   [mbChatManager launchChatForUser:user.userId fromViewController:self];
+   //[mbChatManager launchChatForUser:user.userId fromViewController:self];
     //individual chat
 //    [mbChatManager launchIndividualChat:user.userId andViewControllerObject:self andWithText:@"Hello, I am interested in your  Camorta Island property."];
     
 //    
-//    UIStoryboard* storyboardM = [UIStoryboard storyboardWithName:@"Applozic"
-//                                  bundle:[NSBundle bundleForClass:ALChatViewController.class]];
-//    UIViewController *launchChat = [storyboardM instantiateViewControllerWithIdentifier:@"LaunchChatFromSimpleViewController"];
-//    [self presentViewController:launchChat animated:YES completion:nil];
-//    
+    UIStoryboard* storyboardM = [UIStoryboard storyboardWithName:@"Applozic"
+                                  bundle:[NSBundle bundleForClass:ALChatViewController.class]];
+    UIViewController *launchChat = [storyboardM instantiateViewControllerWithIdentifier:@"LaunchChatFromSimpleViewController"];
+    [self presentViewController:launchChat animated:YES completion:nil];
+    
 }
 
 //-------------------------------------------------------------------------------------------------------------------
