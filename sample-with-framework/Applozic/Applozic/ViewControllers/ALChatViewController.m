9ccf252a596fa6d58e68a1358022c74c7c1d7194
abhishek <abhishekthapliyal7579@gmail.com>
2016-04-29 15:56:16 +0530

    Color + refresh View + tab bar issue #266
    offline location bug

--- sample-with-framework/Applozic/Applozic/ViewControllers/ALChatViewController.m
+++ sample-with-framework/Applozic/Applozic/ViewControllers/ALChatViewController.m
@@ -1919,7 +1919,9 @@ ALMessageDBService  * dbService;
             [self handleErrorStatus:theMessage];
             return;
         }
+        [[self.alMessageWrapper getUpdatedMessageArray] addObject:theMessage];
         [self.mTableView reloadData];
+        [self scrollTableViewToBottomWithAnimation:YES];
         [self setRefreshMainView:TRUE];
     }];
     
